986eebd45f1231183c9058692936612f613914bf
gizmolo <jacob.wallace@utexas.edu>
2025-08-13 11:06:11 -0500

    successfully hitting flask endpoint from python script

---
+++ test.py
@@ -0,0 +1,12 @@
+import httpx
+
+url = "http://localhost:8001/send_data"
+
+data = '''
+{
+    "text": "This is test #2"
+}
+'''
+
+response = httpx.post(url, data=data, headers={"Content-Type": "application/json"})
+print(response.json())
