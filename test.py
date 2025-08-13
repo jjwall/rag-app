8e03a8a14ba1fcfdcc10ddd11f327e76bccf1119
gizmolo <jacob.wallace@utexas.edu>
2025-08-13 11:45:32 -0500

    basic rag app up and running

--- test.py
+++ test.py
@@ -1,11 +1,36 @@
 import httpx
 import ollama
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+local_model = 'llama3.2'
+embedding_model = 'nomic-embed-text'
+
+def load_vector_db():
+    data_to_load = ["The sky is yellow."]
+    vector_db = Chroma.from_texts(data_to_load, OllamaEmbeddings(model=embedding_model))
+    retriever = vector_db.as_retriever()
+    question = "What color is the sky?"
+    docs = retriever.invoke(question)
+    context = "\n\n".join(doc.page_content for doc in docs) # docs.page_content
+    print(context)
+
+    formatted_prompt = f"""Answer the question based ONLY on the following context:
+    {context}
+    Question: {question}
+    """
+
+    response = ollama.chat(model='llama3.2', messages=[{'role': 'user', 'content': formatted_prompt}])
+    print(response['message']['content'])
+
+load_vector_db()
+
 
 def run_agent():
     inference = ollama.chat(model='llama3.2', messages=[{'role': 'user', 'content': 'Why is the sky blue?'}])
     print(inference)
 
-run_agent()
+# run_agent()
 
 def get_data():
     url1 = "http://localhost:8001/send_data"
